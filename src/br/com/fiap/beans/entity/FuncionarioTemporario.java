77a730b25d12339d7f465fc27580ad86fc93bfd3
Samara Porto Souza <samaraporto1301@gmail.com>
2026-03-22 22:20:51 -0300

    Refactor FuncionarioTemporario class structure

--- src/br/com/fiap/beans/entity/FuncionarioTemporario.java
+++ src/br/com/fiap/beans/entity/FuncionarioTemporario.java
@@ -1,4 +1,4 @@
-package br.com.fiap.beans.entity; // Se a sua pasta for diferente, ajuste aqui!
+package br.com.fiap.beans.entity; 
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -8,15 +8,12 @@ public class FuncionarioTemporario extends Funcionario {
 
     @Id
     private Long id;
-    // Bônus fixo para contratos temporários
     private double bonus;
 
-    // Construtor vazio (OBRIGATÓRIO pro JPA)
     public FuncionarioTemporario() {}
 
-    // Construtor com os 4 argumentos que o Main está pedindo
     public FuncionarioTemporario(String nome, int horas, double valorHora, double bonus) {
-        super(nome, horas, valorHora); // Chama o construtor da classe pai (Funcionario)
+        super(nome, horas, valorHora); 
         this.bonus = bonus;
     }
 
