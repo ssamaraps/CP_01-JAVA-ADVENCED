43b65a2aab8ed8006b61cb4435b005d3d919c77c
Samara Porto Souza <samaraporto1301@gmail.com>
2026-03-22 22:19:28 -0300

    Remove unnecessary comments and clean up code

--- src/br/com/fiap/beans/Reflection/GeradorSQL.java
+++ src/br/com/fiap/beans/Reflection/GeradorSQL.java
@@ -1,7 +1,7 @@
 package br.com.fiap.beans.Reflection;
 
 import br.com.fiap.annotation.Descricao;
-import java.lang.reflect.Field; // Import obrigatório para a API Reflection funcionar
+import java.lang.reflect.Field; 
 
 public class GeradorSQL {
 
@@ -9,23 +9,20 @@ public class GeradorSQL {
 
         String nomeTabela;
 
-        // Verifica se a classe tem a anotação @Descricao
         if (classe.isAnnotationPresent(Descricao.class)) {
             Descricao desc = classe.getAnnotation(Descricao.class);
-            nomeTabela = desc.descricao(); // Pega o nome que você definiu na anotação
+            nomeTabela = desc.descricao(); 
         } else {
-            nomeTabela = classe.getSimpleName().toUpperCase(); // Coloquei maiúsculo para ficar padrão de banco Oracle
+            nomeTabela = classe.getSimpleName().toUpperCase();
         }
 
         StringBuilder sql = new StringBuilder("SELECT ");
 
-        // Pega todos os atributos (campos) da classe via Reflection
         Field[] campos = classe.getDeclaredFields();
 
         for (int i = 0; i < campos.length; i++) {
             sql.append(campos[i].getName());
 
-            // Coloca a vírgula, menos no último campo
             if (i < campos.length - 1) {
                 sql.append(", ");
             }
